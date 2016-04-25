9341cf2a8096b2d80221de1c0d03df80a803644a
David Gonzalez <davidgonzalez@currencyfair.com>
2016-04-25 01:04:55 +0100

    Added readme and few examples.

--- examples/basic-round-robin/round-robin.js
+++ examples/basic-round-robin/round-robin.js
@@ -4,6 +4,7 @@ var visigoth = require('../visigoth')();
 visigoth.add('upstream1');
 visigoth.add('upstream2');
 
+// Now visigoth should alternate between them.
 for (var i = 0; i < 100; i++) {
   visigoth.choose(function(upstream, errored, stats) {
       console.log(upstream);
--- examples/econ-refused/econ-refused.js
+++ examples/econ-refused/econ-refused.js
@@ -2,20 +2,15 @@ var visigoth = require('../../visigoth')();
 var request = require('request');
 var async = require('async');
 
-function handleRequest(request, response) {
-    response.end('Here we are!');
-}
-
-
 // Add two endpoints, localhost will fail as no one is listening in there...
 visigoth.add("http://localhost:1233");
 visigoth.add("http://www.google.com");
 
 
-// First, show 
+// First, show all the nodes and their status.
 console.log(visigoth.upstreams$);
 
-
+// Choose a node 3 times, 1 will fail and the two others will be the successful node:
 async.series([
     function(callback) {
         visigoth.choose(function(target, errored, stats) {
@@ -60,5 +55,6 @@ async.series([
         });
     }
 ], function(err, result) {
+    // Show the node list in after all the executions:
     console.log(visigoth.upstreams$);
 });
